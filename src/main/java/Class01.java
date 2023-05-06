eb00d0edceba89ad96710e4744edae645629bbdb
Alp Bulut <alpbulut.software@gmail.com>
2023-05-06 21:48:39 +0300

    v4

--- src/main/java/Class01.java
+++ src/main/java/Class01.java
@@ -1,8 +1,13 @@
 public class Class01 {
     public static void main(String[] args) {
+
         //V1 çalışmalarım...
         //V2 çalışmalarım...
         System.out.println("v3 kodları.."); //v3
+        //v4 kısa yoldan commit
+        //v5 kısa yoldan commit
+        //hatalı kodlar
+
 
 
 
